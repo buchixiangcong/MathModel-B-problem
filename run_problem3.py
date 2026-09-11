4521eb5fad6afe8ef218d55e052b25f26bbc43e5
Minyu Li <3351346914@qq.com>
2026-09-11 11:39:40 +0800

    Enable route-aware optimization in fast mode

--- run_problem3.py
+++ run_problem3.py
@@ -46,6 +46,7 @@ def strategy_config(fast: bool) -> StrategyConfig:
         source_edge_subdivisions=4,
         source_interior_levels=2,
         error_sample_count=3,
+        route_candidate_tracks=3,
     )
 
 
